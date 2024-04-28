668d76448c15a2160c27a557b92a109d096e6205
y.tkachenko <y.tkachenko@mobidev.biz>
2024-04-28 12:13:54 +0300

    test new script

--- generate_version.sh
+++ generate_version.sh
@@ -28,27 +28,24 @@ do
      esac
 done
 
-DEFAULT_PRODUCTION_BRANCH=main
+DEFAULT_PRODUCTION_BRANCH=stable
 CURRENT_BRANCH=`git symbolic-ref --short HEAD`
 SHORT_GIT_HASH=`git rev-parse --short HEAD`
 NEAREST_GIT_TAG=`git describe --tags --candidates 1 $SHORT_GIT_HASH --always | awk -F- '{print $1}'`
-#GIT_TAGS=$(git tag --points-at "$SHORT_GIT_HASH")
-
-# Filter out tags with "stable" in the name
-#NEAREST_GIT_TAG=$(echo "$GIT_TAGS" | grep -v -i 'stable')
 
 if [ -z $PRODUCTION_VERSION ]; then
     if [ "$CURRENT_BRANCH" ]; then
-        VERSION=$CURRENT_BRANCH
+        VERSION=$CURRENT_BRANCH+$SHORT_GIT_HASH
     else
-        #VERSION=$SHORT_GIT_HASH
-        VERSION=$NEAREST_GIT_TAG
+        VERSION=$SHORT_GIT_HASH
     fi
 else
     if [ "$CURRENT_BRANCH" = "$DEFAULT_PRODUCTION_BRANCH" ]; then
-        VERSION=$NEAREST_GIT_TAG
+        VERSION=$NEAREST_GIT_TAG+$SHORT_GIT_HASH
+    elif [ "$CURRENT_BRANCH" ]; then
+        VERSION=$NEAREST_GIT_TAG-$CURRENT_BRANCH+$SHORT_GIT_HASH
     else
-        VERSION=$CURRENT_BRANCH
+        VERSION=$NEAREST_GIT_TAG+$SHORT_GIT_HASH
     fi
 fi
 echo $VERSION
