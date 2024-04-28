0b77f2f1256991ee01883a869d9bf81dd6cd95ff
y.tkachenko <y.tkachenko@mobidev.biz>
2024-04-28 19:13:27 +0300

    run new script

--- generate_version.sh
+++ generate_version.sh
@@ -31,7 +31,12 @@ done
 DEFAULT_PRODUCTION_BRANCH=main
 CURRENT_BRANCH=`git symbolic-ref --short HEAD`
 SHORT_GIT_HASH=`git rev-parse --short HEAD`
-NEAREST_GIT_TAG=`git describe --tags --candidates 1 $SHORT_GIT_HASH --always | awk -F- '{print $1}'`
+#NEAREST_GIT_TAG=`git describe --tags --candidates 1 $SHORT_GIT_HASH --always | awk -F- '{print $1}'`
+# Fetch all tags that point to the specified commit
+tags=`git describe --tags --candidates 1 $SHORT_GIT_HASH --always | awk -F- '{print $1}'`
+
+# Filter out tags with "stable" in the name
+NEAREST_GIT_TAG==$(echo "$tags" | grep -v -i 'stable')
 
 if [ -z $PRODUCTION_VERSION ]; then
     if [ "$CURRENT_BRANCH" ]; then
