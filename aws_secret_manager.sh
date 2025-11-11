e39b2905edc86ff7ad17bc2f8c6e2b52e54cda56
y.tkachenko <y.tkachenko@mobidev.biz>
2025-11-11 18:41:09 +0200

    added script

---
+++ aws_secret_manager.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+fetch_secrets() {
+    SECRET_NAME=$1
+    OUTPUT_FILE=$2
+
+    echo "Fetching secrets for: $SECRET_NAME"
+
+
+    # Get the secret value
+    SECRET_VALUE=$(aws secretsmanager get-secret-value --secret-id $SECRET_NAME --query 'SecretString' --output text)
+
+    if [ $? -ne 0 ]; then
+        echo "Error fetching secret: $SECRET_NAME"
+        exit 1
+    fi
+
+    echo $SECRET_VALUE | jq -r 'to_entries | .[] | "\(.key)=\(.value)"' >> $OUTPUT_FILE
+
+    if [ $? -ne 0 ]; then
+        echo "Error writing to file: $OUTPUT_FILE"
+        exit 1
+    fi
+
+    echo "Secrets for $SECRET_NAME written to $OUTPUT_FILE"
+}
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 SECRET_NAME OUTPUT_FILE"
+    exit 1
+fi
+
+SECRET_NAME=$1
+OUTPUT_FILE=$2
+
+if ! command -v jq &> /dev/null; then
+    echo "jq is not installed. Please install jq to proceed."
+    exit 1
+fi
+
+fetch_secrets $SECRET_NAME $OUTPUT_FILE
